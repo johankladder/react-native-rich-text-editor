0651de921647179881cdf77468859685dde660a4
Johan Kladder <johankladder@Johans-MacBook-Pro.local>
2018-11-22 10:28:16 +0100

    * Refactored tests

--- __tests__/EntityCreator/EntityCreator-test-multipleEntities.test.js
+++ __tests__/EntityCreator/EntityCreator-test-multipleEntities.test.js
@@ -8,10 +8,8 @@ test('Test two entities created when found and after each other', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('<b>test</b><b>test</b>')
     expect(baseContent).toBe('testtest');
     expect(entityMapper).toHaveLength(2);
-    expect(entityMapper[0].startIndex).toBe(0);
-    expect(entityMapper[0].endIndex).toBe(7);
-    expect(entityMapper[1].startIndex).toBe(11);
-    expect(entityMapper[1].endIndex).toBe(18);
+    testEntityWithValues(entityMapper[0], 0, 7, '<b>', '</b>');
+    testEntityWithValues(entityMapper[1], 11, 18, '<b>', '</b>');
 
 });
 
@@ -19,42 +17,37 @@ test('Test two entity created , but with two different tags', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('<i>test</i><b>test</b>')
     expect(baseContent).toBe('testtest');
     expect(entityMapper).toHaveLength(2);
-    expect(entityMapper[0].startIndex).toBe(0);
-    expect(entityMapper[0].endIndex).toBe(7);
-    expect(entityMapper[0].openTag).toBe('<i>');
-    expect(entityMapper[0].closeTag).toBe('</i>');
-    expect(entityMapper[1].startIndex).toBe(11);
-    expect(entityMapper[1].endIndex).toBe(18);
-    expect(entityMapper[1].openTag).toBe('<b>');
-    expect(entityMapper[1].closeTag).toBe('</b>');
+    testEntityWithValues(entityMapper[0], 0, 7, '<i>', '</i>');
+    testEntityWithValues(entityMapper[1], 11, 18, '<b>', '</b>');
 });
 
 test('Test two entities are created when surrounding normal text', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('<b>test</b> other test <b>test</b>');
     expect(baseContent).toBe('test other test test');
     expect(entityMapper).toHaveLength(2);
-    expect(entityMapper[0].startIndex).toBe(0);
-    expect(entityMapper[0].endIndex).toBe(7);
-    expect(entityMapper[1].startIndex).toBe(23);
-    expect(entityMapper[1].endIndex).toBe(30);
+    testEntityWithValues(entityMapper[0], 0, 7, '<b>', '</b>');
+    testEntityWithValues(entityMapper[1], 23, 30, '<b>', '</b>');
 });
 
 test('Test two entities created when text is in front', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('other test <b>test</b><b>test</b>');
     expect(baseContent).toBe('other test testtest');
     expect(entityMapper).toHaveLength(2);
-    expect(entityMapper[0].startIndex).toBe(11);
-    expect(entityMapper[0].endIndex).toBe(18);
-    expect(entityMapper[1].startIndex).toBe(22);
-    expect(entityMapper[1].endIndex).toBe(29);
+    testEntityWithValues(entityMapper[0], 11, 18, '<b>', '</b>');
+    testEntityWithValues(entityMapper[1], 22, 29, '<b>', '</b>');
 });
 
 test('Test two entities are created when surrounding by other text', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('other test <b>test</b><b>test</b> other test');
     expect(baseContent).toBe('other test testtest other test');
     expect(entityMapper).toHaveLength(2);
-    expect(entityMapper[0].startIndex).toBe(11);
-    expect(entityMapper[0].endIndex).toBe(18);
-    expect(entityMapper[1].startIndex).toBe(22);
-    expect(entityMapper[1].endIndex).toBe(29);
+    testEntityWithValues(entityMapper[0], 11, 18, '<b>', '</b>');
+    testEntityWithValues(entityMapper[1], 22, 29, '<b>', '</b>');
 });
+
+const testEntityWithValues = (entity, startIndex, endIndex, openTag, closeTag) => {
+    expect(entity.startIndex).toBe(startIndex);
+    expect(entity.endIndex).toBe(endIndex);
+    expect(entity.openTag).toBe(openTag);
+    expect(entity.closeTag).toBe(closeTag);
+};
--- __tests__/EntityCreator/EntityCreator-test-singleEntity.test.js
+++ __tests__/EntityCreator/EntityCreator-test-singleEntity.test.js
@@ -8,18 +8,14 @@ test('Test one entity created when one supported is found', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('<b>test</b>')
     expect(baseContent).toBe('test');
     expect(entityMapper).toHaveLength(1);
-    expect(entityMapper[0].startIndex).toBe(0);
-    expect(entityMapper[0].endIndex).toBe(7);
+    testEntityWithValues(entityMapper[0], 0, 7, '<b>', '</b>');
 });
 
 test('Test one entity created when one other supported is found', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('<i>test</i>')
     expect(baseContent).toBe('test');
     expect(entityMapper).toHaveLength(1);
-    expect(entityMapper[0].startIndex).toBe(0);
-    expect(entityMapper[0].endIndex).toBe(7);
-    expect(entityMapper[0].openTag).toBe('<i>');
-    expect(entityMapper[0].closeTag).toBe('</i>');
+    testEntityWithValues(entityMapper[0], 0, 7, '<i>', '</i>');
 });
 
 test('Test no entity created when not one is found', () => {
@@ -32,22 +28,26 @@ test('Test one entity created when one is found and other content', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('<b>test</b> other test');
     expect(baseContent).toBe('test other test');
     expect(entityMapper).toHaveLength(1);
-    expect(entityMapper[0].startIndex).toBe(0);
-    expect(entityMapper[0].endIndex).toBe(7);
+    testEntityWithValues(entityMapper[0], 0, 7, '<b>', '</b>');
 });
 
 test('Test one entity created when one is found and other content in front', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('other test <b>test</b>');
     expect(baseContent).toBe('other test test');
     expect(entityMapper).toHaveLength(1);
-    expect(entityMapper[0].startIndex).toBe(11);
-    expect(entityMapper[0].endIndex).toBe(18);
+    testEntityWithValues(entityMapper[0], 11, 18, '<b>', '</b>');
 });
 
 test('Test one entity created when one is found and others around', () => {
     let {baseContent, entityMapper} = this.entityCreator.createFromRichText('other test <b>test</b> other test');
     expect(baseContent).toBe('other test test other test');
     expect(entityMapper).toHaveLength(1);
-    expect(entityMapper[0].startIndex).toBe(11);
-    expect(entityMapper[0].endIndex).toBe(18);
+    testEntityWithValues(entityMapper[0], 11, 18, '<b>', '</b>');
 });
+
+const testEntityWithValues = (entity, startIndex, endIndex, openTag, closeTag) => {
+    expect(entity.startIndex).toBe(startIndex);
+    expect(entity.endIndex).toBe(endIndex);
+    expect(entity.openTag).toBe(openTag);
+    expect(entity.closeTag).toBe(closeTag);
+};
